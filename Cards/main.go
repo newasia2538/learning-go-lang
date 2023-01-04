bcec481793d2d75d10004133be0f99994ddc81f6
Athiporn Chantachai <Athiporn.Chantachai@krungsri.com>
2023-01-04 17:22:32 +0700

    create custom type

--- Cards/main.go
+++ Cards/main.go
@@ -4,14 +4,27 @@ import (
 	"fmt"
 )
 
+// declare variable out of function
 var c string
 
 func main() {
-	fmt.Println("CARDS !!!!")
+	// declare variable and initialize value
 	card := newCard()
+	// declare slice of string type and initailize value
+	cards := []string{"Ace of Diamonds", newCard()}
+	deck := deck{"Ace of Diamonds", newCard()}
+	// add new record into slice
+	cards = append(cards, "Six of Spades")
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+	deck.print()
 	fmt.Println(card)
 }
 
+// declare function return string type value
 func newCard() string {
 	return "Five of Diamonds"
 }
